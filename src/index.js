417d63a66b1687b3b08f2b1382e8173ab1d12eb4
monaco89 <nick.monaco15@gmail.com>
2019-02-20 21:51:43 -0500

    check if test db

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
-import { ApolloServer, AuthenticationError } from 'apollo-server-express';
+import {
+  ApolloServer,
+  AuthenticationError,
+} from 'apollo-server-express';
 import morgan from 'morgan';
 import jwt from 'jsonwebtoken';
 import http from 'http';
@@ -17,66 +20,66 @@ app.use(cors());
 app.use(morgan('dev'));
 
 const getMe = async req => {
-    const token = req.headers['x-token'];
+  const token = req.headers['x-token'];
 
-    if (token) {
-        try {
-            return await jwt.verify(token, process.env.SECRET);
-        } catch (e) {
-            // TODO Fix Error, was breaking site, throws 400 error
-            // throw new AuthenticationError(
-            //     'Your session expired. Sign in again.',
-            // );
-        }
+  if (token) {
+    try {
+      return await jwt.verify(token, process.env.SECRET);
+    } catch (e) {
+      // TODO Fix Error, was breaking site, throws 400 error
+      // throw new AuthenticationError(
+      //     'Your session expired. Sign in again.',
+      // );
     }
+  }
 };
 
 const server = new ApolloServer({
-    typeDefs: schema,
-    resolvers,
-    formatError: error => {
-        const message = error.message
-            .replace('SequelizeValidationError: ', '')
-            .replace('Validation error: ', '');
+  typeDefs: schema,
+  resolvers,
+  formatError: error => {
+    const message = error.message
+      .replace('SequelizeValidationError: ', '')
+      .replace('Validation error: ', '');
 
-        return {
-            ...error,
-            message,
-        };
-    },
-    context: async ({ req }) => {
-        const me = await getMe(req);
+    return {
+      ...error,
+      message,
+    };
+  },
+  context: async ({ req }) => {
+    const me = await getMe(req);
 
-        return {
-            models,
-            me,
-            secret: process.env.SECRET,
-        };
-    },
+    return {
+      models,
+      me,
+      secret: process.env.SECRET,
+    };
+  },
 });
 
 server.applyMiddleware({ app, path: '/graphql' });
 
-const eraseDatabaseOnSync = true;
+// const eraseDatabaseOnSync = true;
+
+const isTest = !!process.env.TEST_DATABASE;
+const isProduction = !!process.env.DATABASE_URL;
 
 const port = process.env.PORT || 8000;
 
-sequelize.sync({ force: eraseDatabaseOnSync }).then(async () => {
-    if (eraseDatabaseOnSync) {
-        createTestUsers();
-    }
+sequelize.sync({ force: isTest }).then(async () => {
+  if (isTest) {
+    createTestUsers();
+  }
 
-    app.listen({ port }, () => {
-        console.log(`Server on http://localhost:${port}/graphql`);
-    });
-
+  app.listen({ port }, () => {
+    console.log(`Server on http://localhost:${port}/graphql`);
+  });
 });
 
 const createTestUsers = async () => {
-    await models.User.create(
-        {
-            email: "nickmonaco@nickmonaco.com",
-            password: "password4321",
-        }
-    );
-}
+  await models.User.create({
+    email: 'nickmonaco@nickmonaco.com',
+    password: 'password4321',
+  });
+};
--- src/models/index.js
+++ src/models/index.js
@@ -1,26 +1,34 @@
 import Sequelize from 'sequelize';
 import spotifyApi from './spotify';
 
-const sequelize = new Sequelize(
-    process.env.DATABASE,
+let sequelize;
+
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'mysql',
+  });
+} else {
+  sequelize = new Sequelize(
+    process.env.TEST_DATABASE || process.env.DATABASE,
     process.env.DATABASE_USER,
     process.env.DATABASE_PASSWORD,
     {
-        dialect: 'mysql',
+      dialect: 'mysql',
     },
-);
+  );
+}
 
 const models = {
-    Spotify: spotifyApi,
-    User: sequelize.import('./user'),
+  Spotify: spotifyApi,
+  User: sequelize.import('./user'),
 };
 
 // const models = spotifyApi;
 
 Object.keys(models).forEach(key => {
-    if ('associate' in models[key]) {
-        models[key].associate(models);
-    }
+  if ('associate' in models[key]) {
+    models[key].associate(models);
+  }
 });
 
 export { sequelize };
