4d905fa4690272fc2ae43dbb702df1d9c6e6d74b
monaco89 <nick.monaco15@gmail.com>
2019-03-10 21:35:57 -0400

    setlist schema

--- src/schema/setlistfm.js
+++ src/schema/setlistfm.js
@@ -1,3 +1,56 @@
 import { gql } from 'apollo-server-express';
 
-export default gql``;
+export default gql`
+  extend type Query {
+    getSetlist(id: String!): Setlist!
+  }
+
+  type Setlist {
+    artist: Artist!
+    venue: Venue!
+    url: String!
+    id: String!
+    eventDate: String!
+    tour: Tour!
+    set: [Set!]!
+  }
+
+  type Artist {
+    mbid: String!
+    name: String!
+    sortName: String!
+  }
+
+  type Venue {
+    city: City!
+    name: String!
+  }
+
+  type Tour {
+    name: String!
+  }
+
+  type Set {
+    name: String!
+    encore: Int
+    song: [Song!]!
+  }
+
+  type City {
+    id: String!
+    name: String!
+    stateCode: String!
+    state: String!
+  }
+
+  type Song {
+    name: String!
+    info: String!
+    cover: Cover
+  }
+
+  type Cover {
+    name: String!
+    artist: Artist!
+  }
+`;
